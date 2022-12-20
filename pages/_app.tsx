acca0cc434dab163782ceab4625248816e441ff7
Dhanushka Chandana <dhanushka787@gmail.com>
2022-12-20 01:18:14 +0100

    Connect API route

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,19 @@
 import type { AppProps } from "next/app";
 import { Inter } from "@next/font/google";
+import Navigation from "../components/navigation";
+import "../styles/globals.css";
+import Footer from "../components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={inter.className}>
-      <Component {...pageProps} />
+    <div className={`${inter.className} flex justify-between flex-col min-h-screen`}>
+      <div>
+        <Navigation />
+        <Component {...pageProps} />
+      </div>
+      <Footer />
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
+import { NextApiRequest } from "next";
 import Head from "next/head";
+import StarItem from "../components/star-item";
+import { StarItem as ItemType } from "../types/star-item";
 
-export default function Home() {
+export default function Home({ items }: { items: ItemType[] }) {
   return (
     <>
       <Head>
@@ -12,9 +15,24 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <h1>Hello</h1>
+      <main className="container mx-auto py-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-10 xl:gap-5 px-10">
+          {items.map((item) => (
+            <StarItem key={item.key} star={item} />
+          ))}
+        </div>
       </main>
     </>
   );
+}
+
+export async function getStaticProps({ req }: { req: NextApiRequest }) {
+  const res = await fetch(`${process.env.BASE_URL}/api/stats`);
+  const { data } = await res.json();
+
+  return {
+    props: {
+      items: data,
+    },
+  };
 }
